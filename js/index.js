5aefe7093510e06ce954edd0874e1c5fc7cf82dd
sandyjernigan <sandy.jernigan@gmail.com>
2019-05-04 13:09:42 -0400

    Added Event Listener mouseover to nav

--- js/index.js
+++ js/index.js
@@ -1,1 +1,26 @@
-// Your code goes here
+// Selectors for Elements
+
+    // Header
+    const headerElement = document.querySelector('header');
+    const headerText = document.querySelector('.logo-heading');
+
+    // Nav List 
+    const navElement = document.querySelector('nav');
+    const NavList = Array.from(document.getElementsByTagName("a"));
+
+// EventListeners
+    // mouseover - When mouse is over the navagation list the elements change color
+    navElement.addEventListener("mouseover", function( event ) {   
+        // highlight the mouseenter target
+        event.target.style.color = "blue";
+
+        // reset the color after a short delay
+        setTimeout(function() {
+            event.target.style.color = "";
+        }, 500);
+        }, false);
+
+
+
+// Test for Errors
+//headerElement.style.backgroundColor = "red";
